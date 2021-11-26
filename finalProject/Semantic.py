d434102865a6d6abf8bfcd0c51021a69b132a575
Donovan Gorrocino <A00344371@itesm.mx>
2021-11-26 09:14:47 +0000

    Added a tree implementation and deleted some unifished code

--- finalProject/Semantic.py
+++ finalProject/Semantic.py
@@ -5,46 +5,35 @@ class SymbolTable:
     vars=dict({})
     id=''
     ancestor=None
-    children=None
-    block=0
-    def __init__(self,id,block,ancestor=None):
+    blk=0
+    def __init__(self,id,blk,ancestor=None):
         self.id=id
         self.ancestor=ancestor
-        self.block=block
+        self.blk=blk
         self.vars=deepcopy(dict({}))
-        if ancestor:
-            ancestor.children=[self]
-            ancestor.children.append(self)
+
 
     def printInfo(self):
         print("id: ",self.id)
         if self.ancestor:
             print("ancestor: ", self.ancestor.id)
         print("Variables: ", self.vars)
-        if self.children:
-            print("children:")
-            for c in self.children:
-                print(c.id)
     
     def addVar(self,name,type):
         self.vars[name]=type
 
-res=None
 
-parseText("example.txt")
-res=absTree
-globalvars = SymbolTable("Global",id(res))
 symTable=[]
 
 def getScopes(id):
-    for tab in symTable:
-        if tab.block==id:
-            return tab
+    for count in symTable:
+        if count.blk==id:
+            return count
     return None
 
-def setScopes(node,parentScope):
+def setScopes(node,ancScope):
     if node:
-        curScp = SymbolTable(node.val,id(node),parentScope)
+        curScp = SymbolTable(node.val,id(node),ancScope)
         symTable.append(curScp)
         for child in node.childrens:
             if child:
@@ -55,10 +44,10 @@ def setScopes(node,parentScope):
                     setScopes(child,curScp)
                 elif child.type == "ELIF":
                     addVars(child.childrens[1],curScp)
-                    setScopes(child,parentScope)
+                    setScopes(child,ancScope)
                 elif child.type == "ELSE":
                     addVars(child.childrens[0],curScp)
-                    setScopes(child,parentScope)
+                    setScopes(child,ancScope)
                 elif child.type == "WHILE":
                     addVars(child.childrens[1],curScp)
                     setScopes(child,curScp)
@@ -124,12 +113,40 @@ def checkVar(var,scope):
         else:
             checkVar(var,scope.ancestor)
 
+#Implementing the tree inside de file, sorry
+res=None
+def generateTestTree():
+    res = Node("Start","Start")
+    decInt=Node("assign","=",[Node("a","INT",var=True),Node("2","INT")])
+    res.childrens.append(decInt)
+    decFlt=Node("assign","=",[Node("b","FLOAT",var=True),Node("3","INT")])
+    res.childrens.append(decFlt)
+    decStr=Node("assign","=",[Node("c","STRING",var=True),Node("tree","STRING")])
+    res.childrens.append(decStr)
+    decElif=Node("elif","ELIF",[Node(">","OPERATION",[Node("a","INT",var=True),Node("2","INT")]),Node("assign","=",[Node("d","BOOLEAN",var=True),Node("true","BOOLEAN")])])
+    decElse=Node("else","ELSE",[Node("block","else",[Node("PRINT","PRINT",[Node("s", "STRING", var=True)])])])
+    decIF=Node("if","IF",[Node("<","OPERATION",[Node("a","FLOAT",var=True),Node("2","INT")]),Node("block","if",[Node("PRINT","PRINT",[Node("b","FLOAT",var=True)])]),decElif,decElse])
+    res.childrens.append(decIF)
+    decI=Node("assign","=",[Node("i","INT",var=True),Node("0","INT")])
+    res.childrens.append(decI)
+    decForStart=Node("assign","=",[Node("i","INT",var=True),Node("0","INT")])
+    decOpFor=Node("<","OPERATION",[Node("i","INT",var=True),Node("5","INT")])
+    decIncFor=Node("+","step",[Node("i","INT",var=True),Node("1","INT")])
+    decForPrint=Node("block","for",[Node("PRINT","PRINT",[Node("i","INT",var=True)])])
+    decFor=Node("for","FOR",[decForStart,decOpFor,decIncFor,decForPrint])
+    res.childrens.append(decFor)
+    decWhile=Node("while","WHILE",[Node("==","OPERATION",[Node("a","INT", var=True),Node("3","INT")]),
+    Node("block","while",[Node("PRINT","PRINT",[Node("c","STRING",var=True)])])])
+    res.childrens.append(decWhile)
+
+    return res
+     
+res=generateTestTree()
+
 setScopes(res,None)
-for tab in symTable:
-    tab.printInfo()
+for count in symTable:
+    count.printInfo()
 print(viewScopes(res,None))
-if not viewScopes(res,None):
-    exit("Missmatch scope")
-print(symTable[0].printInfo())
+
 print("Semantic Analisys done")
 
