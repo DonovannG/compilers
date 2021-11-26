162acc93ba16929ef15ac6707b18a14d0a9fc551
Donovan Gorrocino <A00344371@itesm.mx>
2021-11-26 09:52:53 +0000

    Added the TAC function

--- finalProject/Tac.py
+++ finalProject/Tac.py
@@ -236,3 +236,58 @@ def forBlockGenerator(node,tmp,temp,j,step,compar):
     blockList.append(x)
     return tmpC+1,tmpBG+1
 
+def tasGenerator(node,filename):
+    file = open("finalProject/"+filename,"w")
+    tmp = 1
+    temp = 1
+    for child in node.childrens:
+        if child.type == "=":
+            tmp=assignGenerator(child,file,tmp)
+        elif child.type == "PRINT":
+            tmp=setPrint(child,file,tmp)
+        elif child.type == "IF":
+            tmpN, k = getStringOp(child.childrens[0],tmp)
+            for ifChild in child.childrens:
+                if ifChild.type == "if":
+                    file.write(k)
+                    tmpA,tmpB=blockGenerator(ifChild,tmpN,temp)
+                    file.write("T"+str(tmpN-1)+" IFGOTO L"+str(tmpB-1 )+"\n")
+                    tmp=tmpA
+                    temp=tmpB
+                elif ifChild.type == "ELIF":
+                    tmpN, k = getStringOp(ifChild.childrens[0],tmp)
+                    for elifChild in ifChild.childrens:
+                        if elifChild.type == "elif":
+                            file.write(k)
+                            tmpA,tmpB=blockGenerator(elifChild,tmpN,temp)
+                            file.write("T"+str(tmpN-1)+" ELSEIFGOTO L"+str(tmpB-1)+"\n")
+                            tmp=tmpA
+                            temp=tmpB
+                elif ifChild.type == "ELSE":
+                    if ifChild.childrens[0]:
+                        tmpA,tmpB=blockGenerator(ifChild.childrens[0],tmpN,temp)
+                        file.write("ELSEGOTO L"+str(tmpB-1)+"\n")
+                        tmp=tmpA
+                        temp=tmpB
+        elif child.type == "WHILE":
+            tmpN, k = getStringOp(child.childrens[0],tmp)
+            file.write(k)
+            tmpA,tmpB=whileBlockGenerator(child.childrens[1],tmpN,temp,tmpN)
+            file.write("T"+str(tmpN-1)+" IFGOTO L"+str(tmpB-1)+"\n")
+            tmp=tmpA
+            temp=tmpB
+        elif child.type == "FOR":
+            tmpC,op=getStrAssign(child.childrens[0],tmp)
+            file.write(op)
+            tmpC,op1=getStringOp(child.childrens[1],tmpC)
+            file.write(op1)
+            tmpA,tmpB=forBlockGenerator(child.childrens[3],tmpC,temp,"T"+str(tmpC-1)+" IFGOTO L"+str(temp)+"\n",child.childrens[2],op1)
+            file.write("T"+str(tmpC-1)+" IFGOTO L"+str(tmpB-1)+"\n")
+            tmp=tmpA
+            temp=tmpB
+    for x in blockList:
+        file.write("\n"+x)
+    file.close()
+
+tasGenerator(res,"tac.txt")
+print("Done!")
